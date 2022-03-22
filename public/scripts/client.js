f7b892dcca2f3cd2036a9f333608af72946279cd
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-03-22 17:11:07 -0400

    getting day function added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -27,24 +27,31 @@ const data = [
     },
     "created_at": 1461113959088
   }
-]
+];
+// function to get number of days ago tweeted
+const getDayDifference = function(previoustime) {
+  const currentDate = new Date();
+  const differencetime = currentDate.getTime() - previoustime;
+  console.log(differencetime);
+  const dayDifference = differencetime / (24 * 60 * 60 * 1000);
+  return Math.round(dayDifference);
+};
 const renderTweets = function(tweets) {
 // loops through tweets
 // calls createTweetElement for each tweet
 // takes return value and appends it to the tweets container
-for(const tweet of tweets){
-  console.log(tweet.user.name);
-  $tweetData=createTweetElement(tweet);
-  $("#tweets-container").append($tweetData);
-}
-}
-const createTweetElement = function(tweetData) { 
+  for (const tweet of tweets) {
+    let $tweetData = createTweetElement(tweet);
+    $("#tweets-container").append($tweetData);
+  }
+};
+const createTweetElement = function(tweetData) {
   /* Your code for creating the tweet element */
   const avatar = tweetData.user.avatars;
-  const name= tweetData.user.name;
-  const handle=tweetData.user.handle;
-  const textinput=tweetData.content.text;
-  const time=tweetData.created_at;
+  const name = tweetData.user.name;
+  const handle = tweetData.user.handle;
+  const textinput = tweetData.content.text;
+  const time = tweetData.created_at;
   let $tweet = `<article class="tweets">
   <header>
     <div class="tweetuser">
@@ -55,7 +62,7 @@ const createTweetElement = function(tweetData) {
   </header>                 
     <div class="tweetTextOld">${textinput}</div> 
     <footer>            
-      <output name="days" class="days" for="tweet-text">${time}</output>
+      <output name="days" class="days" for="tweet-text">${getDayDifference(time)} days ago</output>
       <div class="icons">
       <i class="fa-solid fa-flag"></i>
       <i class="fa-solid fa-retweet"></i>
@@ -65,8 +72,8 @@ const createTweetElement = function(tweetData) {
    </article>`;
   console.log($tweet);
   return $tweet;
-}
-$(document).ready(function(){
+};
+$(document).ready(function() {
   renderTweets(data);
 });
   
