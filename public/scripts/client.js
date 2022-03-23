064ff86e865b03ec75207374e2d117cdae7106b1
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-03-23 15:31:30 -0400

    Refetch tweets on Submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,49 +4,14 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-
-// Fake data taken from initial-tweets.json
-/*const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-*/
-// function to get number of days ago tweeted
-// const getDayDifference = function(previoustime) {
-//   const currentDate = new Date();
-//   const differencetime = currentDate.getTime() - previoustime;
-//   //console.log(differencetime);
-//   const dayDifference = differencetime / (24 * 60 * 60 * 1000);
-//   return Math.round(dayDifference);
-// };
 const renderTweets = function(tweets) {
 // loops through tweets
 // calls createTweetElement for each tweet
 // takes return value and appends it to the tweets container
+  //tweets.reverse();
   for (const tweet of tweets) {
     let $tweetData = createTweetElement(tweet);
-    $("#tweets-container").append($tweetData);
+    $("#tweets-container").prepend($tweetData);
   }
 };
 const createTweetElement = function(tweetData) {
@@ -84,6 +49,8 @@ const loadTweets = function() {
     method: 'GET',
     dataType: 'json',
     success: (tweets) => {
+      const $tweets=$('.tweets');
+      $tweets.remove();
       renderTweets(tweets);
     },
     error: (err) => {
@@ -113,10 +80,14 @@ $(document).ready(function() {
       alert("Tweet content is too long");
     } else {
       //tweet will be posted after validation
+    
       const serializedData = $(this).serialize();
       $.post('/tweets', serializedData, response => {
         console.log(response);
-      });
+        loadTweets();
+        $form[0].reset();      //clearing the form text input value
+        $('.counter').val(140); //clearing the form counter value
+      });      
     }
   });
 });
